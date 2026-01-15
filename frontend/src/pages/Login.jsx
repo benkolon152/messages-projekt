aaf80fb8db38812656d5a72a59a6402a4442a16a
Benkolon152 <v.benasz@gmail.com>
2026-01-15 14:06:39 +0100

    Login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+export default function Login() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>Login</h2>
+
+        <input placeholder="Username" />
+        <input placeholder="Password" type="password" />
+
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
