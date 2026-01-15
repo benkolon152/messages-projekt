70366e2a51af457a759475817ca27033dd1b47e0
Benkolon152 <v.benasz@gmail.com>
2026-01-15 14:09:00 +0100

    inbox layout

---
+++ frontend/src/pages/Inbox.jsx
@@ -0,0 +1,17 @@
+import "./Inbox.css";
+
+export default function Inbox() {
+  return (
+    <div className="container">
+      <h2>Inbox</h2>
+
+      <div className="message left">
+        Hello! 👋
+      </div>
+
+      <div className="message right">
+        Hi there! 🙂
+      </div>
+    </div>
+  );
+}
