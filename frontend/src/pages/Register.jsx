9e4bd1eca055fc163ee286c7e243f8956fddf22e
Benkolon152 <v.benasz@gmail.com>
2026-01-15 14:08:02 +0100

    register page

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,14 @@
+export default function Register() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>Register</h2>
+
+        <input placeholder="Username" />
+        <input placeholder="Password" type="password" />
+
+        <button>Create account</button>
+      </div>
+    </div>
+  );
+}
